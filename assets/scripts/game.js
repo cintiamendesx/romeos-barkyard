061821e0e11f36050c3840257b79481ba1dc214c
Cintia Mendes <cintiamendesx@gmail.com>
2022-02-24 19:27:25 -0300

    tentativa

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -25,8 +25,41 @@ function removeDisplayNone() {
 }
 
 
-const sortImages = document.getElementsByClassName("emotion");
+/*const sortImages = document.getElementsByClassName("emotion");
 for (let i = 0; i < emotion.length; i++) {
   console.log(emotion[i]);
-  emotion[i].addEventListener('click', removeDisplayNone);
-}
+  emotion[i].addEventListener('click', removeDisplayNone);*/
+
+
+    /*function getRandomImage () {
+       let items = $('div.emotionsFigures emotion').length;
+       let rnd   = Math.floor(Math.random() * items);
+        
+       document.getElementsByClassName("emotion").style.display = "block";
+    };*/
+
+
+var elems  = [document.getElementsByClassName("emotionsFigures"),document.getElementsByClassName("emotionsFigures2"),document.getElementsByClassName("emotionsFigures3")];
+for(var i=0;i<elems.lenght;i++)
+elems[i].style.display="none";
+var randomvalue = Math.floor(Math.random() * elems.length);
+elems[randomvalue].style.display="inherit";
+
+
+
+emotionsFigures.innerHTML = randomvalue;
+
+
+
+
+
+/*
+apendar componente
+Math.random
+indice aleatorio
+for 
+multiplica pelo tamanho da array - math floor 
+todas elas evento click 
+random index a cada loop 
+ao inves de (i) - random index
+de - para */
